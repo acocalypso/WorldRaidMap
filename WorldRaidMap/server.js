a0d838f01a856dcda981e5aedb0b027a62c99686
Aco <st.luzifer6663@htomail.com>
2022-02-10 15:40:58 +0100

    parse some values from webhook

--- WorldRaidMap/server.js
+++ WorldRaidMap/server.js
@@ -1,6 +1,7 @@
 // Require express and body-parser
 const express = require("express")
 const bodyParser = require("body-parser")
+const wc = require('which-country');
 // Initialize express and define a port
 const app = express()
 const PORT = 6525
@@ -11,6 +12,9 @@ app.listen(PORT, () => console.log(`?? Server running on port ${PORT}`))
 
 app.use(bodyParser.json())
 app.post("/hook", (req, res) => {
-    console.log(req.body) // Call your action on the request here
     res.status(200).end() // Responding is important
+    console.log(wc([req.body.latitude, req.body.longitude]))
+    console.log(req.body.name)
+    console.log(req.body.level)
+    console.log(req.body.pokemon_id)
 })
